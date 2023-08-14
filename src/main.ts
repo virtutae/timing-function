05ad47f8ede2d3f1949a3f0f9d1224f7a2f89f40
virtutae <virtutae@yahoo.it>
2023-08-14 11:26:46 +0100

    commit at the end of the excercise

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,48 @@
-import { sum } from "./skeleton";
+/*Measure how long it takes to `array.push` a number onto an array of the following lengths.  
+Store your data in a notion database or a spreadsheet at sheets.google.com.
+10 numbers
+100 numbers
+1_000 numbers
+10_000 numbers
+100_000 numbers
+1_000_000 numbers
+10_000_000 numbers
+100_000_000 numbers
+*/
 
-// Call your function of interest, here, instead of sum.
-//You can use vscode's "Debug main.ts" launcher to debug, starting here.
+let arr:number[] = [];
 
-const answer = sum(10, 200);
+function addToArr (arr:number[]): number[] {
+    for (let i = 0; i < 10; i++){
+        arr.push(i)
+    }
+    return arr
+}
+//array.push()
+// function timeCount(addToArr:number[]):void {
+//     const startTime = performance.now()
+//     addToArr.push(1)
+//     const stopTime = performance.now()
+//     const elapsedTime = stopTime - startTime;
+//     console.log({elapsedTime})
+// } 
 
-console.log({ answer });
+//array.unshift()
+// function timeCount(addToArr:number[]):void {
+//     const startTime = performance.now()
+//     addToArr.unshift(1)
+//     const stopTime = performance.now()
+//     const elapsedTime = stopTime - startTime;
+//     console.log({elapsedTime})
+// }
+
+//array.pop()
+function timeCount(addToArr:number[]):void {
+    addToArr.unshift(1,312,312,31234,12,13,123,123,1234,123,12,412,432,4,234,234,234,2354,23,423,42)
+}
+const startTime = performance.now()
+timeCount(arr)
+const stopTime = performance.now()
+const elapsedTime = stopTime - startTime;
+
+console.log({elapsedTime})
